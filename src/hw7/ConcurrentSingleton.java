b242f84aecb13d76e20be0af7547e0e8ee71d927
Annie Moriondo <anniemoriondo@gmail.com>
2021-11-11 16:33:54 -0500

    finished HW 7

--- src/hw7/ConcurrentSingleton.java
+++ src/hw7/ConcurrentSingleton.java
@@ -2,7 +2,7 @@ package hw7;
 
 import java.util.concurrent.locks.ReentrantLock;
 
-public class ConcurrentSingleton {
+public class ConcurrentSingleton implements Runnable{
 
     private ConcurrentSingleton(){}
 
@@ -10,6 +10,10 @@ public class ConcurrentSingleton {
     private static ConcurrentSingleton instance = null;
 
     private static ReentrantLock lock = new ReentrantLock();
+
+    public void run(){
+        System.out.println(getInstance());
+    }
 
     // Static factory method
     public static ConcurrentSingleton getInstance(){
@@ -24,8 +28,28 @@ public class ConcurrentSingleton {
     }
 
     public static void main(String[] args){
+        ConcurrentSingleton cs1 = new ConcurrentSingleton();
+        ConcurrentSingleton cs2 = new ConcurrentSingleton();
+        ConcurrentSingleton cs3 = new ConcurrentSingleton();
+
+        System.out.println(
+                "All three threads should be accessing the same instance.");
+
+        // Multiple threads
+        Thread t1 = new Thread(cs1);
+        Thread t2 = new Thread(cs2);
+        Thread t3 = new Thread(cs3);
+
+        // Run multiple threads
+        t1.start();
+        t2.start();
+        t3.start();
+
+        try {
+            t1.join();
+            t2.join();
+            t3.join();
+        } catch (InterruptedException e){e.printStackTrace();}
 
     }
-
-
 }
