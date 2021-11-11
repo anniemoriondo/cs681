185dce3c4f7452356c51af17b683abdb597a2837
Annie Moriondo <anniemoriondo@gmail.com>
2021-11-11 17:35:40 -0500

    Link methods made thread safe

--- src/hw8/Link.java
+++ src/hw8/Link.java
@@ -9,33 +9,60 @@ public class Link extends FSElement {
                  FSElement target){
         // File size of link is always 0
         super(parent, name, 0, creationTime);
-        this.target = target;
-        if (parent != null){ parent.appendChild(this); }
+        // Thready safe access to LinkedList
+        lock.lock();
+        try {
+            this.target = target;
+            if (parent != null){ parent.appendChild(this); }
+        } finally {
+            lock.unlock();
+        }
     }
 
+    // Already atomic
     public boolean isDirectory() { return false; }
 
+    // Already atomic
     public boolean isLink() { return true; }
 
     public FSElement getTarget(){ return target; }
 
     // Info about target
-    public String targetName(){ return target.getName(); }
+    public String targetName(){
+        // Thread-safe access
+        lock.lock();
+        try{return target.getName(); } finally { lock.unlock(); }
+    }
 
     public String targetLocation(){
+        // Thread-safe access
+        lock.lock();
+        try {
         return target.getParent() != null ?
                 target.getParent().getName() : null;
+        } finally { lock.unlock(); }
     }
 
     public int targetSize(){
         // Returns target's total size if it's a directory; else return its size
-        return target.isDirectory() ?
+        // Thread-safe acces
+        lock.lock();
+        try {
+            return target.isDirectory() ?
                 ((Directory)target).getTotalSize() : target.getSize();
+        } finally { lock.unlock(); }
     }
 
-    public boolean targetIsDirectory(){ return target.isDirectory(); }
+    public boolean targetIsDirectory(){
+        // Thread-safe access
+        lock.lock();
+        try {return target.isDirectory(); } finally { lock.unlock(); }
+    }
 
-    public boolean targetIsLink(){return target.isLink();}
+    public boolean targetIsLink(){
+        // Thread-safe access
+        lock.lock();
+        try {return target.isLink(); } finally { lock.unlock(); }}
 
 
 }
