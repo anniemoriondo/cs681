6048f37f561ddf5a3bdec3745821aa1067ef2fd6
Annie Moriondo <anniemoriondo@gmail.com>
2021-11-11 14:57:34 -0500

    fixed hw5 testing

--- src/hw5/RunnableCancellablePrimeGenerator.java
+++ src/hw5/RunnableCancellablePrimeGenerator.java
@@ -1,10 +1,12 @@
 package hw5;
 
+import hw6.RunnableCancellablePrimeFactorizer;
+
 import java.util.concurrent.locks.ReentrantLock;
 
 public class RunnableCancellablePrimeGenerator extends RunnablePrimeGenerator {
     private boolean done = false;
-    private ReentrantLock lock = new ReentrantLock();
+    ReentrantLock lock = new ReentrantLock();
 
     public RunnableCancellablePrimeGenerator(long from, long to) {
         super(from, to);
@@ -35,16 +37,23 @@ public class RunnableCancellablePrimeGenerator extends RunnablePrimeGenerator {
     }
 
     public static void main(String[] args) {
+        System.out.println("Cancellation test:");
         RunnableCancellablePrimeGenerator gen = new RunnableCancellablePrimeGenerator(1,100);
+        RunnableCancellablePrimeGenerator gen2 = new RunnableCancellablePrimeGenerator(1, 1000);
         Thread thread = new Thread(gen);
+        Thread thread2 = new Thread(gen2);
         thread.start();
+        thread2.start();
         gen.setDone();
         try {
             thread.join();
+            thread2.join();
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
         gen.getPrimes().forEach( (Long prime)-> System.out.print(prime + ", ") );
-        System.out.println("\n" + gen.getPrimes().size() + " prime numbers are found.");
+        System.out.println("\n" + gen.getPrimes().size() + " prime numbers are found from 1 to 100.");
+        gen2.getPrimes().forEach( (Long prime)-> System.out.print(prime + ", ") );
+        System.out.println("\n" + gen2.getPrimes().size() + " prime numbers are found from 1 to 1000.");
     }
 }
