e3b2101c613b28cd8b1d1d4aef4150709c719388
Annie Moriondo <anniemoriondo@gmail.com>
2021-11-11 17:52:23 -0500

    adding test code in main of FileSystem

--- src/hw8/FileSystem.java
+++ src/hw8/FileSystem.java
@@ -4,12 +4,14 @@ package hw8;
 import java.util.LinkedList;
 import java.util.concurrent.locks.ReentrantLock;
 
-public class FileSystem {
+public class FileSystem implements Runnable {
     private static FileSystem instance = null;
     private static ReentrantLock lock = new ReentrantLock();
     private LinkedList<Directory> rootDirs = new LinkedList<>();
 
     private FileSystem(){}
+
+    public void run(){}
 
     public static FileSystem getFileSystem(){
         lock.lock();
@@ -37,6 +39,32 @@ public class FileSystem {
         try { this.rootDirs.add(root); } finally { lock.unlock();
         }
     }
+
+    public static void main(String[] args){
+        // Verify concurrent singleton
+        FileSystem fs1 = new FileSystem();
+        FileSystem fs2 = new FileSystem();
+
+        // Multiple threads should all be accessing same instance
+        System.out.println("Same instance?");
+        Thread t1 = new Thread(fs1);
+        Thread t2 = new Thread(fs2);
+
+        t1.start();
+        t2.start();
+
+        try {
+            t1.join();
+            t2.join();
+
+            System.out.println(fs1.getFileSystem());
+            System.out.println(fs2.getFileSystem());
+        } catch (InterruptedException e){
+            e.printStackTrace();
+        }
+    }
+
+
 
 }
 
