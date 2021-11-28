be34f265fa4b038f14b76baa6c5651b1b1ec65ac
Annie Moriondo <anniemoriondo@gmail.com>
2021-11-27 19:02:02 -0500

    Address class for HW 11

---
+++ src/hw11/Address.java
@@ -0,0 +1,35 @@
+package hw11;
+
+public final class Address {
+    private final String street, city, state;
+    private final int zipcode;
+
+    public Address(String street, String city, String state, int zipcode){
+        this.street = street;
+        this.city = city;
+        this.state = state;
+        this.zipcode = zipcode;
+    }
+
+    // Getter methods
+    public String getStreet(){ return street; }
+    public String getCity(){ return city; }
+    public String getState(){ return state; }
+    public int getZipcode() { return zipcode; }
+
+    // The whole address as a string.
+    public String toString(){
+        return street + ", " + city + " " + state + " " + zipcode;
+    }
+
+    // Compares two Addresses
+    public boolean equals(Address otherAddress){
+        return this.toString().equals(otherAddress.toString());
+    }
+
+    public static void main(String[] args){
+        Address whiteHouse = new Address("1600 Pennsylvania Avenue",
+                "Washington", "DC", 20500);
+        System.out.println(whiteHouse);
+    }
+}
